5b3b3a0eed59ab0398938e44f0433dc4d0eaf198
sentayehut <tsentayehu4@gmail.com>
2021-11-03 05:08:43 -0700

    bulit in file

--- bulltin.c
+++ bulltin.c
@@ -1,183 +1,152 @@
 #include "shell.h"
+/**
+ * exit_bul - Exit Statue Shell
+ * @cmd: Parsed Command
+ * @input: User Input
+ * @argv:Program Name
+ * @c:Excute Count
+ * Return: Void (Exit Statue)
+ */
+void  exit_bul(char **cmd, char *input, char **argv, int c)
+{
+int statue, i = 0;
+
+if (cmd[1] == NULL)
+{
+free(input);
+free(cmd);
+exit(EXIT_SUCCESS);
+}
+while (cmd[1][i])
+{
+if (_isalpha(cmd[1][i++]) != 0)
+{
+_prerror(argv, c, cmd);
+break;
+}
+else
+{
+statue = _atoi(cmd[1]);
+free(input);
+free(cmd);
+exit(statue);
+}
+}
+}
+
 
 /**
- * _which - function that show the command which in shell
- * @source: doble pointer
- * @environ: list of vars
- * @test: path
- * @count_cmd: count of commands ls
- * Return: 1 in case of successful
+ * change_dir - Change Dirctorie
+ * @cmd: Parsed Command
+ * @er: Statue Last Command Excuted
+ * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
  */
-int _which(char **source, char **environ, dir **test, long int *count_cmd)
+int change_dir(char **cmd, __attribute__((unused))int er)
 {
-	int j = 0, i = 0, count_list = 0;
-	struct stat st;
-	dir *copia = *test;
-	char **verificar;
+int value = -1;
+char cwd[PATH_MAX];
 
-	(void)environ;
-	(void)count_cmd;
-	for (count_list = 0; copia; copia = copia->next, count_list++)
-	{
-	}
-	count_list++;
-	if (source[1] == NULL)
-		return (1);
-	if (*source[1] != '/' && *source[1] != 46)
-	{
-		verificar = _verification(test, source[1], &count_list);
-		for (j = 0; verificar[j]; j++)
-		{
-			if (stat(verificar[j], &st) != -1)
-			{
-				for (i = 0; *(verificar[j] + i); i++)
-				{}
-				write(1, verificar[j], i);
-				write(1, "\n", 1);
-				break;
-			}
-		}
-		free_function(verificar, &count_list);
-	}
-	else
-	{
-		if ((stat(source[1], &st)) != -1)
-		{
-			for (i = 0; *(source[1] + i); i++)
-			{}
-			write(1, source[1], i);
-			write(1, "\n", 1);
-		}
-	}
-	return (1);
+if (cmd[1] == NULL)
+value = chdir(getenv("HOME"));
+else if (_strcmp(cmd[1], "-") == 0)
+{
+value = chdir(getenv("OLDPWD"));
+}
+else
+value = chdir(cmd[1]);
+
+if (value == -1)
+{
+perror("hsh");
+return (-1);
+}
+else if (value != -1)
+{
+getcwd(cwd, sizeof(cwd));
+setenv("OLDPWD", getenv("PWD"), 1);
+setenv("PWD", cwd, 1);
+}
+return (0);
 }
 /**
- * _cd - function that show the command cd in shell
- * @source: doble pointer
- * @environ:list of vars ls
- * @test:path
- * @count_cmd:count of commands
- * Return: 1 in case of successful
+ * dis_env - Display Enviroment Variable
+ * @cmd:Parsed Command
+ * @er:Statue of Last command Excuted
+ * Return:Always 0
  */
-int _cd(char **source, char **environ, dir **test, long int *count_cmd)
+int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
 {
-	char s[100];
-	char *msg = NULL;
-	int sizenum = 0;
+size_t i;
+int len;
 
-	(void)environ;
-	(void)test;
-	getcwd(s, 100);
-	if (source[1] == NULL)
-		return (1);
-	if (chdir(source[1]) != 0)
-	{
-		msg = _union(count_cmd, &sizenum);
-		perror(msg);
-		free(msg);
-	}
-	return (1);
+for (i = 0; environ[i] != NULL; i++)
+{
+len = _strlen(environ[i]);
+write(1, environ[i], len);
+write(STDOUT_FILENO, "\n", 1);
+}
+return (0);
 }
 /**
- * _help - function that show the command help in shell
- * @source: doble pointer
- * @environ: this is the
- * @test:path
- * @count_cmd:count of commands
- * Return: 1 in case of successful
+ * display_help - Displaying Help For Builtin
+ * @cmd:Parsed Command
+ * @er: Statue Of Last Command Excuted
+ * Return: 0 Succes -1 Fail
  */
-int _help(char **source, char **environ, dir **test, long int *count_cmd)
+int display_help(char **cmd, __attribute__((unused))int er)
 {
-	ssize_t fd, read_data = 0;
-	char concatenar[6] = "help_";
-	char *archivo = '\0';
-	char buf[1024];
-	int i = 0, j = 0, total = 0;
+int fd, fw, rd = 1;
+char c;
 
-	(void)environ;
-	(void)test;
-	(void)count_cmd;
-	if (source[1] == NULL)
-		return (1);
-	for (i = 0; *(source[1] + i); i++)
-	{}
-	i++;
-	total = i + 5;
-	archivo = malloc(sizeof(char) * (total));
-	if (archivo == '\0')
-		return (0);
-	for (j = 0; concatenar[j]; j++)
-		archivo[j] = concatenar[j];
-	for (j = 0; *(source[1] + j); j++)
-		archivo[5 + j] = *(source[1] + j);
-	archivo[5 + j] = '\0';
-	fd = open(archivo, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	while ((read_data = read(fd, buf, 1024)) > 0)
-	{
-		write(STDOUT_FILENO, buf, read_data);
-	}
-	close(fd);
-	free(archivo);
-	return (1);
+fd = open(cmd[1], O_RDONLY);
+if (fd < 0)
+{
+perror("Error");
+return (0);
+}
+while (rd > 0)
+{
+rd = read(fd, &c, 1);
+fw = write(STDOUT_FILENO, &c, rd);
+if (fw < 0)
+{
+return (-1);
+}
+}
+_putchar('\n');
+return (0);
 }
 /**
- * _env - function that show the command env in shell
- * @source: doble pointer
- * @environ: this is the
- * @test:path
- * @count_cmd:count of commands
- * Return: 1 in case of successful
+ * echo_bul - Excute Echo Cases
+ * @st:Statue Of Last Command Excuted
+ * @cmd: Parsed Command
+ * Return: Always 0 Or Excute Normal Echo
  */
-int _env(char **source, char **environ, dir **test, long int *count_cmd)
+int echo_bul(char **cmd, int st)
 {
-	int i = 0, j = 0;
-	char *env = NULL;
-	(void)source;
-	(void)test;
-	(void)count_cmd;
+char *path;
+unsigned int  pid = getppid();
 
-	if (environ == NULL || *environ == NULL)
-	{
-		write(1, "not found enviromental variable\n", 32);
-		return (1);
-	}
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		env = environ[i];
-		for (j = 0; env[j]; j++)
-		{}
-		write(STDOUT_FILENO, env, j);
-		write(STDOUT_FILENO, "\n", 2);
-	}
-	return (1);
+if (_strncmp(cmd[1], "$?", 2) == 0)
+{
+print_number_in(st);
+PRINTER("\n");
 }
-/**
- * _union - function that show the command union in shell
- * @sizenum: sizeof
- * @count_cmd:count of commands
- * Return: total
- */
-char *_union(long int *count_cmd, int *sizenum)
+else if (_strncmp(cmd[1], "$$", 2) == 0)
 {
-	int i = 0;
-	char *total = NULL;
-	char *num = NULL;
-	char inicio[5] = "sh: ";
-	char inter[5] = ": cd";
+print_number(pid);
+PRINTER("\n");
 
-	num = print_integers(count_cmd, sizenum);
-	total = malloc(sizeof(char) * (9 + *sizenum));
-	if (total == '\0')
-		return ('\0');
-	for (i = 0; inicio[i]; i++)
-		total[i] = inicio[i];
-	for (i = 0; i < *sizenum; i++)
-		total[4 + i] = num[i];
-	for (i = 0; inter[i]; i++)
-		total[4 + *sizenum + i] = inter[i];
-	total[4 + *sizenum + i] = '\0';
-	free(num);
-	return (total);
+}
+else if (_strncmp(cmd[1], "$PATH", 5) == 0)
+{
+path = _getenv("PATH");
+PRINTER(path);
+PRINTER("\n");
+free(path);
+}
+else
+return (print_echo(cmd));
+
+return (1);
 }
