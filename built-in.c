d28b8a1ddd38423ca59f34a97d8eec02c30e2ac3
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-11-03 07:50:08 -0700

    commit

--- built-in.c
+++ built-in.c
@@ -8,12 +8,12 @@
  */
 void built_in(char *string, char **argv, char **env, int *ex_it)
 {
-  if (_strcmp(argv[0], "exit") == 0)
-    {
-      free(argv);
-      free(string);
-      exit(*ex_it);
-    }
-  if (_strcmp(argv[0], "env") == 0)
-    printenv(env, ex_it);
+if (_strcmp(argv[0], "exit") == 0)
+{
+free(argv);
+free(string);
+exit(*ex_it);
+}
+if (_strcmp(argv[0], "env") == 0)
+printenv(env, ex_it);
 }
