60f8467219044d086a896d71678f5a8a0a920228
sentayehut <tsentayehu4@gmail.com>
2021-11-02 23:18:01 -0700

    bulitinfile

--- bulltin.c
+++ bulltin.c
@@ -1,151 +1,68 @@
 #include "shell.h"
-/**
- * exit_bul - Exit Statue Shell
- * @cmd: Parsed Command
- * @input: User Input
- * @argv:Program Name
- * @c:Excute Count
- * Return: Void (Exit Statue)
- */
-void  exit_bul(char **cmd, char *input, char **argv, int c)
-{
-int statue, i = 0;
-
-if (cmd[1] == NULL)
-{
-free(input);
-free(cmd);
-exit(EXIT_SUCCESS);
-}
-while (cmd[1][i])
-{
-if (_isalpha(cmd[1][i++]) != 0)
-{
-_prerror(argv, c, cmd);
-break;
-}
-else
-{
-statue = _atoi(cmd[1]);
-free(input);
-free(cmd);
-exit(statue);
-}
-}
-}
 
 /**
- * change_dir - Change Dirctorie
- * @cmd: Parsed Command
- * @er: Statue Last Command Excuted
- * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
- */
-int change_dir(char **cmd, __attribute__((unused))int er)
-{
-int value = -1;
-char cwd[PATH_MAX];
-
-if (cmd[1] == NULL)
-value = chdir(getenv("HOME"));
-else if (_strcmp(cmd[1], "-") == 0)
-{
-value = chdir(getenv("OLDPWD"));
-}
-else
-value = chdir(cmd[1]);
-
-if (value == -1)
-{
-perror("hsh");
-return (-1);
-}
-else if (value != -1)
-{
-getcwd(cwd, sizeof(cwd));
-setenv("OLDPWD", getenv("PWD"), 1);
-setenv("PWD", cwd, 1);
-}
-return (0);
-}
-/**
- * dis_env - Display Enviroment Variable
- * @cmd:Parsed Command
- * @er:Statue of Last command Excuted
- * Return:Always 0
- */
-int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
-{
-size_t i;
-int len;
-
-for (i = 0; environ[i] != NULL; i++)
-{
-len = _strlen(environ[i]);
-write(1, environ[i], len);
-write(STDOUT_FILENO, "\n", 1);
-}
-return (0);
-}
-/**
- * display_help - Displaying Help For Builtin
- * @cmd:Parsed Command
- * @er: Statue Of Last Command Excuted
+ * history_dis - Display History Of User Input Simple Shell
+ * @c:Parsed Command
+ * @s:Statue Of Last Excute
  * Return: 0 Succes -1 Fail
  */
-int display_help(char **cmd, __attribute__((unused))int er)
+int history_dis(__attribute__((unused))char **c, __attribute__((unused))int s)
 {
-int fd, fw, rd = 1;
-char c;
+	char *filename = ".simple_shell_history";
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	int counter = 0;
+	char *er;
 
-fd = open(cmd[1], O_RDONLY);
-if (fd < 0)
-{
-perror("Error");
-return (0);
-}
-while (rd > 0)
-{
-rd = read(fd, &c, 1);
-fw = write(STDOUT_FILENO, &c, rd);
-if (fw < 0)
-{
-return (-1);
-}
-}
-_putchar('\n');
-return (0);
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+	while ((getline(&line, &len, fp)) != -1)
+	{
+		counter++;
+		er = _itoa(counter);
+		PRINTER(er);
+		free(er);
+		PRINTER(" ");
+		PRINTER(line);
+
+	}
+	if (line)
+		free(line);
+	fclose(fp);
+	return (0);
 }
 /**
- * echo_bul - Excute Echo Cases
- * @st:Statue Of Last Command Excuted
+ * print_echo - Excute Normal Echo
  * @cmd: Parsed Command
- * Return: Always 0 Or Excute Normal Echo
+ * Return: 0 Succes -1 Fail
  */
-int echo_bul(char **cmd, int st)
+int print_echo(char **cmd)
 {
-char *path;
-unsigned int  pid = getppid();
+	pid_t pid;
+	int status;
 
-if (_strncmp(cmd[1], "$?", 2) == 0)
-{
-print_number_in(st);
-PRINTER("\n");
-}
-else if (_strncmp(cmd[1], "$$", 2) == 0)
-{
-print_number(pid);
-PRINTER("\n");
+	pid = fork();
+	if (pid == 0)
+	{
+	if (execve("/bin/echo", cmd, environ) == -1)
+	{
+		return (-1);
+	}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
 
-}
-else if (_strncmp(cmd[1], "$PATH", 5) == 0)
-{
-path = _getenv("PATH");
-PRINTER(path);
-PRINTER("\n");
-free(path);
-}
-else
-return (print_echo(cmd));
-
-return (1);
-}
