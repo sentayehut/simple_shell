dccd21b9759578eb70ea97d2187cc044e3bde138
sentayehut <tsentayehu4@gmail.com>
2021-11-03 05:38:53 -0700

    bulit and shell

--- bulltin.c
+++ bulltin.c
@@ -1,152 +1,192 @@
 #include "shell.h"
-/**
- * exit_bul - Exit Statue Shell
- * @cmd: Parsed Command
- * @input: User Input
- * @argv:Program Name
- * @c:Excute Count
- * Return: Void (Exit Statue)
- */
-void  exit_bul(char **cmd, char *input, char **argv, int c)
-{
-int statue, i = 0;
-
-if (cmd[1] == NULL)
-{
-free(input);
-free(cmd);
-exit(EXIT_SUCCESS);
-}
-while (cmd[1][i])
-{
-if (_isalpha(cmd[1][i++]) != 0)
-{
-_prerror(argv, c, cmd);
-break;
-}
-else
-{
-statue = _atoi(cmd[1]);
-free(input);
-free(cmd);
-exit(statue);
-}
-}
-}
-
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
 /**
- * change_dir - Change Dirctorie
- * @cmd: Parsed Command
- * @er: Statue Last Command Excuted
- * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
+ * get_builtin - Matches a command with a corresponding
+ *               shellby builtin function.
+ * @command: The command to match.
+ *
+ * Return: A function pointer to the corresponding builtin.
  */
-int change_dir(char **cmd, __attribute__((unused))int er)
+int (*get_builtin(char *command))(char **args, char **front)
 {
-int value = -1;
-char cwd[PATH_MAX];
+	builtin_t funcs[] = {
+		{ "exit", shellby_exit },
+		{ "env", shellby_env },
+		{ "setenv", shellby_setenv },
+		{ "unsetenv", shellby_unsetenv },
+		{ "cd", shellby_cd },
+		{ "alias", shellby_alias },
+		{ "help", shellby_help },
+		{ NULL, NULL }
+	};
+	int i;
 
-if (cmd[1] == NULL)
-value = chdir(getenv("HOME"));
-else if (_strcmp(cmd[1], "-") == 0)
-{
-value = chdir(getenv("OLDPWD"));
+	for (i = 0; funcs[i].name; i++)
+	{
+		if (_strcmp(funcs[i].name, command) == 0)
+			break;
+	}
+	return (funcs[i].f);
 }
-else
-value = chdir(cmd[1]);
 
-if (value == -1)
-{
-perror("hsh");
-return (-1);
-}
-else if (value != -1)
-{
-getcwd(cwd, sizeof(cwd));
-setenv("OLDPWD", getenv("PWD"), 1);
-setenv("PWD", cwd, 1);
-}
-return (0);
-}
 /**
- * dis_env - Display Enviroment Variable
- * @cmd:Parsed Command
- * @er:Statue of Last command Excuted
- * Return:Always 0
+ * shellby_exit - Causes normal process termination
+ *                for the shellby shell.
+ * @args: An array of arguments containing the exit value.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If there are no arguments - -3.
+ *         If the given exit value is invalid - 2.
+ *         O/w - exits with the given status value.
+ *
+ * Description: Upon returning -3, the program exits back in the main function.
  */
-int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
+int shellby_exit(char **args, char **front)
 {
-size_t i;
-int len;
+	int i, len_of_int = 10;
+	unsigned int num = 0, max = 1 << (sizeof(int) * 8 - 1);
 
-for (i = 0; environ[i] != NULL; i++)
-{
-len = _strlen(environ[i]);
-write(1, environ[i], len);
-write(STDOUT_FILENO, "\n", 1);
+	if (args[0])
+	{
+		if (args[0][0] == '+')
+		{
+			i = 1;
+			len_of_int++;
+		}
+		for (; args[0][i]; i++)
+		{
+			if (i <= len_of_int && args[0][i] >= '0' && args[0][i] <= '9')
+				num = (num * 10) + (args[0][i] - '0');
+			else
+				return (create_error(--args, 2));
+		}
+	}
+	else
+	{
+		return (-3);
+	}
+	if (num > max - 1)
+		return (create_error(--args, 2));
+	args -= 1;
+	free_args(args, front);
+	free_env();
+	free_alias_list(aliases);
+	exit(num);
 }
-return (0);
-}
+
 /**
- * display_help - Displaying Help For Builtin
- * @cmd:Parsed Command
- * @er: Statue Of Last Command Excuted
- * Return: 0 Succes -1 Fail
+ * shellby_cd - Changes the current directory of the shellby process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If the given string is not a directory - 2.
+ *         If an error occurs - -1.
+ *         Otherwise - 0.
  */
-int display_help(char **cmd, __attribute__((unused))int er)
+int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 {
-int fd, fw, rd = 1;
-char c;
+	char **dir_info, *new_line = "\n";
+	char *oldpwd = NULL, *pwd = NULL;
+	struct stat dir;
 
-fd = open(cmd[1], O_RDONLY);
-if (fd < 0)
-{
-perror("Error");
-return (0);
+	oldpwd = getcwd(oldpwd, 0);
+	if (!oldpwd)
+		return (-1);
+
+	if (args[0])
+	{
+		if (*(args[0]) == '-' || _strcmp(args[0], "--") == 0)
+		{
+			if ((args[0][1] == '-' && args[0][2] == '\0') ||
+					args[0][1] == '\0')
+			{
+				if (_getenv("OLDPWD") != NULL)
+					(chdir(*_getenv("OLDPWD") + 7));
+			}
+			else
+			{
+				free(oldpwd);
+				return (create_error(args, 2));
+			}
+		}
+		else
+		{
+			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode)
+					&& ((dir.st_mode & S_IXUSR) != 0))
+				chdir(args[0]);
+			else
+			{
+				free(oldpwd);
+				return (create_error(args, 2));
+			}
+		}
+	}
+	else
+	{
+		if (_getenv("HOME") != NULL)
+			chdir(*(_getenv("HOME")) + 5);
+	}
+
+	pwd = getcwd(pwd, 0);
+	if (!pwd)
+		return (-1);
+
+	dir_info = malloc(sizeof(char *) * 2);
+	if (!dir_info)
+		return (-1);
+
+	dir_info[0] = "OLDPWD";
+	dir_info[1] = oldpwd;
+	if (shellby_setenv(dir_info, dir_info) == -1)
+		return (-1);
+
+	dir_info[0] = "PWD";
+	dir_info[1] = pwd;
+	if (shellby_setenv(dir_info, dir_info) == -1)
+		return (-1);
+	if (args[0] && args[0][0] == '-' && args[0][1] != '-')
+	{
+		write(STDOUT_FILENO, pwd, _strlen(pwd));
+		write(STDOUT_FILENO, new_line, 1);
+	}
+	free(oldpwd);
+	free(pwd);
+	free(dir_info);
+	return (0);
 }
-while (rd > 0)
-{
-rd = read(fd, &c, 1);
-fw = write(STDOUT_FILENO, &c, rd);
-if (fw < 0)
-{
-return (-1);
-}
-}
-_putchar('\n');
-return (0);
-}
+
 /**
- * echo_bul - Excute Echo Cases
- * @st:Statue Of Last Command Excuted
- * @cmd: Parsed Command
- * Return: Always 0 Or Excute Normal Echo
+ * shellby_help - Displays information about shellby builtin commands.
+ * @args: An array of arguments.
+ * @front: A pointer to the beginning of args.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - 0.
  */
-int echo_bul(char **cmd, int st)
+int shellby_help(char **args, char __attribute__((__unused__)) **front)
 {
-char *path;
-unsigned int  pid = getppid();
+	if (!args[0])
+		help_all();
+	else if (_strcmp(args[0], "alias") == 0)
+		help_alias();
+	else if (_strcmp(args[0], "cd") == 0)
+		help_cd();
+	else if (_strcmp(args[0], "exit") == 0)
+		help_exit();
+	else if (_strcmp(args[0], "env") == 0)
+		help_env();
+	else if (_strcmp(args[0], "setenv") == 0)
+		help_setenv();
+	else if (_strcmp(args[0], "unsetenv") == 0)
+		help_unsetenv();
+	else if (_strcmp(args[0], "help") == 0)
+		help_help();
+	else
+		write(STDERR_FILENO, name, _strlen(name));
 
-if (_strncmp(cmd[1], "$?", 2) == 0)
-{
-print_number_in(st);
-PRINTER("\n");
-}
-else if (_strncmp(cmd[1], "$$", 2) == 0)
-{
-print_number(pid);
-PRINTER("\n");
-
-}
-else if (_strncmp(cmd[1], "$PATH", 5) == 0)
-{
-path = _getenv("PATH");
-PRINTER(path);
-PRINTER("\n");
-free(path);
-}
-else
-return (print_echo(cmd));
-
-return (1);
+	return (0);
 }
