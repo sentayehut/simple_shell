98a664cdeb5f87d3a0f3706e7219e7de6a8081d9
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-11-05 13:15:19 -0700

    proc_file_commands

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -5,7 +5,7 @@ int proc_file_commands(char *file_path, int *exe_ret);
  * cant_open - If the file doesn't exist or lacks proper permissions, print
  * a cant open error.
  * @file_path: Path to the supposed file.
- *
+ * author : Anteneh Bizuneh & Sentayehu Tilahun
  * Return: 127.
  */
 int cant_open(char *file_path)
