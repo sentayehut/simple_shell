15d3da3ce2ee2345ded8e7ecebf48bf69d80e3f1
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-11-05 12:59:40 -0700

    _getline - Reads input from a stream.

--- getline.c
+++ getline.c
@@ -11,6 +11,7 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
  *         Otherwise - a pointer to the reallocated memory block.
+ * author: Anteneh Kassaw & Sentayehu Tilahun
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
