8e52b362c5f8828f53aa4d62f9a898eafb0ca8a3
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-11-05 12:35:47 -0700

    builtin function

--- builtin.c
+++ builtin.c
@@ -7,7 +7,7 @@ int shellby_help(char **args, char __attribute__((__unused__)) **front);
  * get_builtin - Matches a command with a corresponding
  *               shellby builtin function.
  * @command: The command to match.
- *
+ * author: Sentayehu Tilahun & Anteneh Bizuneh
  * Return: A function pointer to the corresponding builtin.
  */
 int (*get_builtin(char *command))(char **args, char **front)
