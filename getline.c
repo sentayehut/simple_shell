97a2720bbefc96174253892be2adcc30c5412e36
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-02-20 10:22:09 -0800

    Adding second parameter front for free_args

--- getline.c
+++ getline.c
@@ -1,7 +1,9 @@
 #include "shell.h"
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
 /**
  * _realloc - Reallocates a memory block using malloc and free.
  * @ptr: A pointer to the memory previously allocated.
@@ -11,7 +13,6 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
  *         Otherwise - a pointer to the reallocated memory block.
- * author: Anteneh Kassaw & Sentayehu Tilahun
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -63,24 +64,29 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
  */
 void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 {
-if (*lineptr == NULL)
-{
-if (b > 120)
-*n = b;
-else
-*n = 120;
-*lineptr = buffer; }
-else if (*n < b)
-{
-if (b > 120)
-*n = b;
-else
-*n = 120;
-*lineptr = buffer; }
-else
-{
-_strcpy(*lineptr, buffer);
-free(buffer); }}
+	if (*lineptr == NULL)
+	{
+		if (b > 120)
+			*n = b;
+		else
+			*n = 120;
+		*lineptr = buffer;
+	}
+	else if (*n < b)
+	{
+		if (b > 120)
+			*n = b;
+		else
+			*n = 120;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+
 /**
  * _getline - Reads input from a stream.
  * @lineptr: A buffer to store the input.
@@ -91,37 +97,47 @@ free(buffer); }}
  */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-static ssize_t input;
-ssize_t ret;
-char c = 'x', *buffer;
-int r;
-if (input == 0)
-fflush(stream);
-else
-return (-1);
-input = 0;
-buffer = malloc(sizeof(char) * 120);
-if (!buffer)
-return (-1);
-while (c != '\n')
-{
-r = read(STDIN_FILENO, &c, 1);
-if (r == -1 || (r == 0 && input == 0))
-{
-free(buffer);
-return (-1); }
-if (r == 0 && input != 0)
-{
-input++;
-break; }
-if (input >= 120)
-buffer = _realloc(buffer, input, input + 1);
-buffer[input] = c;
-input++; }
-buffer[input] = '\0';
-assign_lineptr(lineptr, n, buffer, input);
-ret = input;
-if (r != 0)
-input = 0;
-return (ret);
+	static ssize_t input;
+	ssize_t ret;
+	char c = 'x', *buffer;
+	int r;
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * 120);
+	if (!buffer)
+		return (-1);
+
+	while (c != '\n')
+	{
+		r = read(STDIN_FILENO, &c, 1);
+		if (r == -1 || (r == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (r == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+
+		if (input >= 120)
+			buffer = _realloc(buffer, input, input + 1);
+
+		buffer[input] = c;
+		input++;
+	}
+	buffer[input] = '\0';
+
+	assign_lineptr(lineptr, n, buffer, input);
+
+	ret = input;
+	if (r != 0)
+		input = 0;
+	return (ret);
 }
