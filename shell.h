02b6615ce5c20a0c73d87d070fa442440d84f266
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-02-20 10:44:43 -0800

    Header file

--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -10,25 +11,28 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+
 #define END_OF_FILE -2
 #define EXIT -3
+
 /* Global environemnt */
 extern char **environ;
 /* Global program name */
 char *name;
 /* Global history counter */
 int hist;
+
 /**
  * struct list_s - A new struct type defining a linked list.
  * @dir: A directory path.
  * @next: A pointer to another struct list_s.
- * author: Anteneh kassaw & Sentayehu Tilahun
  */
 typedef struct list_s
 {
-char *dir;
-struct list_s *next;
+	char *dir;
+	struct list_s *next;
 } list_t;
+
 /**
  * struct builtin_s - A new struct type defining builtin commands.
  * @name: The name of the builtin command.
@@ -36,9 +40,10 @@ struct list_s *next;
  */
 typedef struct builtin_s
 {
-char *name;
-int (*f)(char **argv, char **front);
+	char *name;
+	int (*f)(char **argv, char **front);
 } builtin_t;
+
 /**
  * struct alias_s - A new struct defining aliases.
  * @name: The name of the alias.
@@ -47,12 +52,14 @@ int (*f)(char **argv, char **front);
  */
 typedef struct alias_s
 {
-char *name;
-char *value;
-struct alias_s *next;
+	char *name;
+	char *value;
+	struct alias_s *next;
 } alias_t;
+
 /* Global aliases linked list */
 alias_t *aliases;
+
 /* Main Helpers */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
@@ -62,6 +69,7 @@ list_t *get_path_dir(char *path);
 int execute(char **args, char **front);
 void free_list(list_t *head);
 char *_itoa(int num);
+
 /* Input Helpers */
 void handle_line(char **line, ssize_t read);
 void variable_replacement(char **args, int *exe_ret);
@@ -72,6 +80,7 @@ int handle_args(int *exe_ret);
 int check_args(char **args);
 void free_args(char **args, char **front);
 char **replace_aliases(char **args);
+
 /* String functions */
 int _strlen(const char *s);
 char *_strcat(char *dest, const char *src);
@@ -81,6 +90,7 @@ char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
+
 /* Builtins */
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
@@ -90,10 +100,12 @@ int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
 int shellby_cd(char **args, char __attribute__((__unused__)) **front);
 int shellby_alias(char **args, char __attribute__((__unused__)) **front);
 int shellby_help(char **args, char __attribute__((__unused__)) **front);
+
 /* Builtin Helpers */
 char **_copyenv(void);
 void free_env(void);
 char **_getenv(const char *var);
+
 /* Error Handling */
 int create_error(char **args, int err);
 char *error_env(char **args);
@@ -103,11 +115,13 @@ char *error_2_cd(char **args);
 char *error_2_syntax(char **args);
 char *error_126(char **args);
 char *error_127(char **args);
+
 /* Linkedlist Helpers */
 alias_t *add_alias_end(alias_t **head, char *name, char *value);
 void free_alias_list(alias_t *head);
 list_t *add_node_end(list_t **head, char *dir);
 void free_list(list_t *head);
+
 void help_all(void);
 void help_alias(void);
 void help_cd(void);
@@ -117,6 +131,6 @@ void help_env(void);
 void help_setenv(void);
 void help_unsetenv(void);
 void help_history(void);
-int proc_file_commands(char *file_path, int *exe_ret);
 
+int proc_file_commands(char *file_path, int *exe_ret);
 #endif /* _SHELL_H_ */
