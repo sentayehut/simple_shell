5e15d5286caf86efe8b65a29ca80036a47a989b4
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-02-20 10:24:15 -0800

    Separating args_helpers to handle;

--- helper.c
+++ helper.c
@@ -1,20 +1,25 @@
 #include "shell.h"
+
 void free_args(char **args, char **front);
 char *get_pid(void);
 char *get_env_value(char *beginning, int len);
 void variable_replacement(char **args, int *exe_ret);
+
 /**
  * free_args - Frees up memory taken by args.
  * @args: A null-terminated double pointer containing commands/arguments.
  * @front: A double pointer to the beginning of args.
- * author : Anteneh Kassaw & Sentayehu Tilahun
  */
 void free_args(char **args, char **front)
 {
-size_t i;
-for (i = 0; args[i] || args[i + 1]; i++)
-free(args[i]);
-free(front); }
+	size_t i;
+
+	for (i = 0; args[i] || args[i + 1]; i++)
+		free(args[i]);
+
+	free(front);
+}
+
 /**
  * get_pid - Gets the current process ID.
  * Description: Opens the stat file, a space-delimited file containing
@@ -26,25 +31,31 @@ free(front); }
  */
 char *get_pid(void)
 {
-size_t i = 0;
-char *buffer;
-ssize_t file;
-file = open("/proc/self/stat", O_RDONLY);
-if (file == -1)
-{
-perror("Cant read file");
-return (NULL); }
-buffer = malloc(120);
-if (!buffer)
-{
-close(file);
-return (NULL); }
-read(file, buffer, 120);
-while (buffer[i] != ' ')
-i++;
-buffer[i] = '\0';
-close(file);
-return (buffer); }
+	size_t i = 0;
+	char *buffer;
+	ssize_t file;
+
+	file = open("/proc/self/stat", O_RDONLY);
+	if (file == -1)
+	{
+		perror("Cant read file");
+		return (NULL);
+	}
+	buffer = malloc(120);
+	if (!buffer)
+	{
+		close(file);
+		return (NULL);
+	}
+	read(file, buffer, 120);
+	while (buffer[i] != ' ')
+		i++;
+	buffer[i] = '\0';
+
+	close(file);
+	return (buffer);
+}
+
 /**
  * get_env_value - Gets the value corresponding to an environmental variable.
  * @beginning: The environmental variable to search for.
@@ -57,26 +68,31 @@ return (buffer); }
  */
 char *get_env_value(char *beginning, int len)
 {
-char **var_addr;
-char *replacement = NULL, *temp, *var;
-var = malloc(len + 1);
-if (!var)
-return (NULL);
-var[0] = '\0';
-_strncat(var, beginning, len);
-var_addr = _getenv(var);
-free(var);
-if (var_addr)
-{
-temp = *var_addr;
-while (*temp != '=')
-temp++;
-temp++;
-replacement = malloc(_strlen(temp) + 1);
-if (replacement)
-_strcpy(replacement, temp);
+	char **var_addr;
+	char *replacement = NULL, *temp, *var;
+
+	var = malloc(len + 1);
+	if (!var)
+		return (NULL);
+	var[0] = '\0';
+	_strncat(var, beginning, len);
+
+	var_addr = _getenv(var);
+	free(var);
+	if (var_addr)
+	{
+		temp = *var_addr;
+		while (*temp != '=')
+			temp++;
+		temp++;
+		replacement = malloc(_strlen(temp) + 1);
+		if (replacement)
+			_strcpy(replacement, temp);
+	}
+
+	return (replacement);
 }
-return (replacement); }
+
 /**
  * variable_replacement - Handles variable replacement.
  * @line: A double pointer containing the command and arguments.
@@ -88,44 +104,52 @@ return (replacement); }
  */
 void variable_replacement(char **line, int *exe_ret)
 {
-int j, k = 0, len;
-char *replacement = NULL, *old_line = NULL, *new_line;
-old_line = *line;
-for (j = 0; old_line[j]; j++)
-{
-if (old_line[j] == '$' && old_line[j + 1] &&
-old_line[j + 1] != ' ')
-{
-if (old_line[j + 1] == '$')
-{
-replacement = get_pid();
-k = j + 2; }
-else if (old_line[j + 1] == '?')
-{
-replacement = _itoa(*exe_ret);
-k = j + 2; }
-else if (old_line[j + 1])
-{
-/* extract the variable name to search for */
-for (k = j + 1; old_line[k] &&
-old_line[k] != '$' &&
-old_line[k] != ' '; k++)
-;
-len = k - (j + 1);
-replacement = get_env_value(&old_line[j + 1], len); }
-new_line = malloc(j + _strlen(replacement)
-+ _strlen(&old_line[k]) + 1);
-if (!line)
-return;
-new_line[0] = '\0';
-_strncat(new_line, old_line, j);
-if (replacement)
-{
-_strcat(new_line, replacement);
-free(replacement);
-replacement = NULL; }
-_strcat(new_line, &old_line[k]);
-free(old_line);
-*line = new_line;
-old_line = new_line;
-j = -1; }}}
+	int j, k = 0, len;
+	char *replacement = NULL, *old_line = NULL, *new_line;
+
+	old_line = *line;
+	for (j = 0; old_line[j]; j++)
+	{
+		if (old_line[j] == '$' && old_line[j + 1] &&
+				old_line[j + 1] != ' ')
+		{
+			if (old_line[j + 1] == '$')
+			{
+				replacement = get_pid();
+				k = j + 2;
+			}
+			else if (old_line[j + 1] == '?')
+			{
+				replacement = _itoa(*exe_ret);
+				k = j + 2;
+			}
+			else if (old_line[j + 1])
+			{
+				/* extract the variable name to search for */
+				for (k = j + 1; old_line[k] &&
+						old_line[k] != '$' &&
+						old_line[k] != ' '; k++)
+					;
+				len = k - (j + 1);
+				replacement = get_env_value(&old_line[j + 1], len);
+			}
+			new_line = malloc(j + _strlen(replacement)
+					  + _strlen(&old_line[k]) + 1);
+			if (!line)
+				return;
+			new_line[0] = '\0';
+			_strncat(new_line, old_line, j);
+			if (replacement)
+			{
+				_strcat(new_line, replacement);
+				free(replacement);
+				replacement = NULL;
+			}
+			_strcat(new_line, &old_line[k]);
+			free(old_line);
+			*line = new_line;
+			old_line = new_line;
+			j = -1;
+		}
+	}
+}
