7d2db5664eb0c6aaec77274ad39af07e9a081d1b
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-02-20 10:48:02 -0800

    Refactoring to use name + hist as global variables

--- str_funcs1.c
+++ str_funcs1.c
@@ -1,23 +1,27 @@
 #include "shell.h"
+
 int _strlen(const char *s);
 char *_strcpy(char *dest, const char *src);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
+
 /**
  * _strlen - Returns the length of a string.
  * @s: A pointer to the characters string.
  *
  * Return: The length of the character string.
- * author :Sentayehu Tilahun & Antenh Bizuneh
  */
 int _strlen(const char *s)
 {
-int length = 0;
-if (!s)
-return (length);
-for (length = 0; s[length]; length++)
-;
-return (length); }
+	int length = 0;
+
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
+		;
+	return (length);
+}
+
 /**
  * _strcpy - Copies the string pointed to by src, including the
  *           terminating null byte, to the buffer pointed by des.
@@ -28,11 +32,14 @@ return (length); }
  */
 char *_strcpy(char *dest, const char *src)
 {
-size_t i;
-for (i = 0; src[i] != '\0'; i++)
-dest[i] = src[i];
-dest[i] = '\0';
-return (dest); }
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
 /**
  * _strcat - Concantenates two strings.
  * @dest: Pointer to destination string.
@@ -42,16 +49,21 @@ return (dest); }
  */
 char *_strcat(char *dest, const char *src)
 {
-char *destTemp;
-const char *srcTemp;
-destTemp = dest;
-srcTemp =  src;
-while (*destTemp != '\0')
-destTemp++;
-while (*srcTemp != '\0')
-*destTemp++ = *srcTemp++;
-*destTemp = '\0';
-return (dest); }
+	char *destTemp;
+	const char *srcTemp;
+
+	destTemp = dest;
+	srcTemp =  src;
+
+	while (*destTemp != '\0')
+		destTemp++;
+
+	while (*srcTemp != '\0')
+		*destTemp++ = *srcTemp++;
+	*destTemp = '\0';
+	return (dest);
+}
+
 /**
  * _strncat - Concantenates two strings where n number
  *            of bytes are copied from source.
@@ -63,9 +75,12 @@ return (dest); }
  */
 char *_strncat(char *dest, const char *src, size_t n)
 {
-size_t dest_len = _strlen(dest);
-size_t i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[dest_len + i] = src[i];
-dest[dest_len + i] = '\0';
-return (dest); }
+	size_t dest_len = _strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
