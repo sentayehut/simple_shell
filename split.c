b64e61b965d2ee4a92d648bf896d017b70ba852c
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-02-20 10:46:10 -0800

    Adding second parameter front for free_args

--- split.c
+++ split.c
@@ -1,7 +1,9 @@
 #include "shell.h"
+
 int token_len(char *str, char *delim);
 int count_tokens(char *str, char *delim);
 char **_strtok(char *line, char *delim);
+
 /**
  * token_len - Locates the delimiter index marking the end
  *             of the first token contained within a string.
@@ -10,16 +12,20 @@ char **_strtok(char *line, char *delim);
  *
  * Return: The delimiter index marking the end of
  *         the intitial token pointed to be str.
- * author :Anteneh Bizuneh & Sentayehu Tilahun
  */
 int token_len(char *str, char *delim)
 {
-int index = 0, len = 0;
-while (*(str + index) && *(str + index) != *delim)
-{
-len++;
-index++; }
-return (len); }
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != *delim)
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
+
 /**
  * count_tokens - Counts the number of delimited
  *                words contained within a string.
@@ -30,16 +36,23 @@ return (len); }
  */
 int count_tokens(char *str, char *delim)
 {
-int index, tokens = 0, len = 0;
-for (index = 0; *(str + index); index++)
-len++;
-for (index = 0; index < len; index++)
-{
-if (*(str + index) != *delim)
-{
-tokens++;
-index += token_len(str + index, delim); }}
-return (tokens); }
+	int index, tokens = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != *delim)
+		{
+			tokens++;
+			index += token_len(str + index, delim);
+		}
+	}
+
+	return (tokens);
+}
+
 /**
  * _strtok - Tokenizes a string.
  * @line: The string.
@@ -49,31 +62,43 @@ return (tokens); }
  */
 char **_strtok(char *line, char *delim)
 {
-char **ptr;
-int index = 0, tokens, t, letters, l;
-tokens = count_tokens(line, delim);
-if (tokens == 0)
-return (NULL);
-ptr = malloc(sizeof(char *) * (tokens + 2));
-if (!ptr)
-return (NULL);
-for (t = 0; t < tokens; t++)
-{
-while (line[index] == *delim)
-index++;
-letters = token_len(line + index, delim);
-ptr[t] = malloc(sizeof(char) * (letters + 1));
-if (!ptr[t])
-{
-for (index -= 1; index >= 0; index--)
-free(ptr[index]);
-free(ptr);
-return (NULL); }
-for (l = 0; l < letters; l++)
-{
-ptr[t][l] = line[index];
-index++; }
-ptr[t][l] = '\0'; }
-ptr[t] = NULL;
-ptr[t + 1] = NULL;
-return (ptr); }
+	char **ptr;
+	int index = 0, tokens, t, letters, l;
+
+	tokens = count_tokens(line, delim);
+	if (tokens == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(char *) * (tokens + 2));
+	if (!ptr)
+		return (NULL);
+
+	for (t = 0; t < tokens; t++)
+	{
+		while (line[index] == *delim)
+			index++;
+
+		letters = token_len(line + index, delim);
+
+		ptr[t] = malloc(sizeof(char) * (letters + 1));
+		if (!ptr[t])
+		{
+			for (index -= 1; index >= 0; index--)
+				free(ptr[index]);
+			free(ptr);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+		{
+			ptr[t][l] = line[index];
+			index++;
+		}
+
+		ptr[t][l] = '\0';
+	}
+	ptr[t] = NULL;
+	ptr[t + 1] = NULL;
+
+	return (ptr);
+}
